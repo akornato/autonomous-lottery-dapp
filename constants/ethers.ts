5379959597523c2290ea63cd4b5ced9d2b15bae7
akornato <artur.kornatowski@gmail.com>
2021-07-12 21:58:33 +0200

    use Hardhat node on SSR in dev

--- constants/ethers.ts
+++ constants/ethers.ts
@@ -1,5 +1,7 @@
 import { ethers } from "ethers";
 
+const { NODE_ENV } = process.env;
+const isDevelopment = NODE_ENV === "development";
 declare global {
   interface Window {
     ethereum: any;
@@ -9,6 +11,8 @@ declare global {
 export const provider =
   typeof window !== "undefined" && window.ethereum
     ? new ethers.providers.Web3Provider(window.ethereum)
+    : isDevelopment
+    ? new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545")
     : new ethers.providers.JsonRpcProvider(
         `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
       );
